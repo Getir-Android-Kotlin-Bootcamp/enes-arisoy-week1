ae2e3b49de6e6fbfc0bd92fc715cf441ed25d82e
Enes Arısoy <arisoyeness@gmail.com>
2024-03-19 16:08:37 +0300

    Page 11 -> Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,6 +31,30 @@ fun main() {
 
     println("-------------------------------------------")
 
+    /**
+     *              Variables
+     *  var -> It's a mutable. Which means it can be changed after it's initialized.
+     *  val -> It's read-only variable. Not immutable. That means you can not write to
+     *  it after initialization.
+     */
+    println("Page 11 -> Variables")
+
+    val name: String = "Enes"
+    println("Name declared with val keyword: $name")
+
+    val list = mutableListOf(1, 2, 3)
+    list.add(4)
+    println(list)
+    /**
+     * list is declared by val keyword. However, you can add an item to list.
+     * This means, val is not immutable but read-only.
+     */
+
+    var sum = 0
+    sum = 2 + 3
+    println("Sum declared with var keyword: $sum. This means the variable can be changed.")
+
+    println("-------------------------------------------")
 
 
 }
