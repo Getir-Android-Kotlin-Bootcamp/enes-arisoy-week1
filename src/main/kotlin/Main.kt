deaad68f50fa3944f594e3d98ad1d989ffc9ccac
Enes Arısoy <arisoyeness@gmail.com>
2024-03-19 15:59:41 +0300

    First commit and explained println

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun main() {
+
+    /**
+     *              Print and Println
+     *  print   -> Prints the given text without appending a new line.
+     *  println -> Prints the given text and appends a new line.
+     */
+    println("Page 9 -> Println")
+
+    println()
+    print("I'm Enes")
+    println("I'm Enes!")
+    println()
+
+    println("-------------------------------------------")
+
+
+}
